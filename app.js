55088128e12f5296f8264a306c0a0bc38995164b
Dalton Junior <daltonneiasjr@hotmail.com>
2026-03-16 22:07:44 -0300

    Atualizando erro numero secreto

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ function verificarChute() {
         document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
         if (chute > numeroSecreto) {
-            exibirTextoNaTela('p', 'O número secrea9sdasd9akskas9dkto é menor');
+            exibirTextoNaTela('p', 'O número secreto é menor');
         } else {
             exibirTextoNaTela('p', 'O número secreto é maior');
         }
